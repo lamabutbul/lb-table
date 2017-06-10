177ca7aa7199f932f0a2e57aaa376475b630ab13
Moshe Abutbul <lamabutbul@gmail.com>
2017-06-11 00:21:16 +0300

    Build v1.0.1

--- dist/lb-table.js
+++ dist/lb-table.js
@@ -146,7 +146,7 @@ angular.module('lbTable').directive('lbTableService', ['$injector', function($in
     function tableStateToFindParams(state) {
         let params = {};
 
-        if (state.sort.length) {
+        if (state.sort && state.sort.length) {
             let sort = [];
             for (let i = 0; i < state.sort.length; i++) {
                 sort.push((state.sort[i][1] > 0 ? '' : '-') + state.sort[i][0]);
@@ -160,7 +160,7 @@ angular.module('lbTable').directive('lbTableService', ['$injector', function($in
     function tableStateToBindParams(state) {
         let params = {};
 
-        if (state.sort.length) {
+        if (state.sort && state.sort.length) {
             let sort = [];
             for (let i = 0; i < state.sort.length; i++) {
                 sort.push([state.sort[i][0], state.sort[i][1] > 0 ? 'ASC' : 'DESC']);
